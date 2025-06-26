94ace31c228951db487a8604a47dff015ca00dbf
ericvanm <eric.vanmeerbeck@gmail.com>
2025-06-26 17:50:59 +0200

    Allow the render host and adapt the name Pokedex in anecdotes for teh Dsicord notifications

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  ppreview: {
+    allowedHosts: [
+      "fullstack-open-par11-based-part6.onrender.com",
+      "localhost",
+    ],
+  },
+});
