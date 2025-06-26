78797a354c3bda4050814b1cc39ff578714ad112
ericvanm <eric.vanmeerbeck@gmail.com>
2025-06-26 15:26:37 +0200

    11.20 Your own pipeline - deploy on render

---
+++ tests/anecdotes.test.js
@@ -0,0 +1,10 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Anecdote", () => {
+  test("Title, filter and create new are on main page", async ({ page }) => {
+    await page.goto("");
+    await expect(page.getByText("filter")).toBeVisible();
+    await expect(page.getByText("Anecdotes")).toBeVisible();
+    await expect(page.getByText("create new")).toBeVisible();
+  });
+});
