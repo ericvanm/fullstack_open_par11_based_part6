767bca139c532807d6919597a8cb52a412e8c5b8
ericvanm <eric.vanmeerbeck@gmail.com>
2025-06-26 18:00:54 +0200

    Typing error

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  ppreview: {
+  preview: {
     allowedHosts: [
       "fullstack-open-par11-based-part6.onrender.com",
       "localhost",
