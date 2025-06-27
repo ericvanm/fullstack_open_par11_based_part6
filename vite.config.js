3374ba569d58d1fb1f1c4beb89e3984465749244
ericvanm <eric.vanmeerbeck@gmail.com>
2025-06-27 10:46:39 +0200

    Use a proxy vite to allow connection in render to the backend

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/anecdotes": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+    },
+  },
   preview: {
     allowedHosts: [
       "fullstack-open-par11-based-part6.onrender.com",
