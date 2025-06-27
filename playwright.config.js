1e163670fdf7dab92254aa46be6cd55b4095fd27
ericvanm <eric.vanmeerbeck@gmail.com>
2025-06-27 12:18:09 +0200

    Try to stabilize the playwright testing (still failing on github)
    The strange behavior is that the tests playwright work fine on the local system but fail on the github....

--- playwright.config.js
+++ playwright.config.js
@@ -73,8 +73,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run preview",
+    command: "npm run preview:full",
     url: "http://localhost:5173",
+    timeout: 120000, // 2 minutes
     reuseExistingServer: !process.env.CI,
   },
 });
