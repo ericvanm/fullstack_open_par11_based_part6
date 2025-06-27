1fe11ef8aa083144f907f935d024b42897d0c87e
ericvanm <eric.vanmeerbeck@gmail.com>
2025-06-27 16:14:20 +0200

    Still an issue with pipeline

--- playwright.config.js
+++ playwright.config.js
@@ -53,7 +53,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run preview:full",
+    command: "npm run preview:ci",
     url: "http://localhost:5173",
     timeout: 120000, // 2 minutes
     reuseExistingServer: !process.env.CI,
