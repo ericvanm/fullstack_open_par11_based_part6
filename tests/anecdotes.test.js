08455c457f81eff9071dbf603f524c790f0543db
ericvanm <eric.vanmeerbeck@gmail.com>
2025-06-27 17:30:08 +0200

    Update anecdotes.test.js

--- tests/anecdotes.test.js
+++ tests/anecdotes.test.js
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Anecdote", () => {
   test("Title, filter and create new are on main page", async ({ page }) => {
-    await page.goto("/");
+    await page.goto("http://localhost:5173/");
     await expect(page.getByText("filter")).toBeVisible();
     await expect(page.getByText("Anecdotes")).toBeVisible();
     await expect(page.getByText("create new")).toBeVisible();
