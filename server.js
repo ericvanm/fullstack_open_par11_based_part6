313b5bcf0ff537f34a0858618081f0aaf75c5dc6
ericvanm <eric.vanmeerbeck@gmail.com>
2025-06-27 19:09:41 +0200

    Create a new server to adapt json-server with cors
    I had to downgrade json-server to 0.17.4 otherwise error

---
+++ server.js
@@ -0,0 +1,28 @@
+const jsonServer = require("json-server");
+const cors = require("cors");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+const allowedOrigins = [
+  "https://fullstack-open-par11-based-part6.onrender.com",
+  "http://localhost:5173",
+];
+
+server.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS: " + origin));
+      }
+    },
+  })
+);
+server.use(middlewares);
+server.use(router);
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
